5c5b98cb26d81279de8ab0b5e5dab0be0c11828e
lesliesync <94773632+lesliesync@users.noreply.github.com>
2021-11-22 01:33:24 +0800

    Update anotherbackup.sh

--- anotherbackup.sh
+++ anotherbackup.sh
@@ -1,4 +1,4 @@
 # Recommended to use OneDrive -> Google Drive and Google Drive -> Google Drive at the same time.
 # Then backup the whole backup drive...
 # Optional, if you don't need that, put a hashtag(#) before the command.
-rclone copy gdrive01: gdrive02: --transfers 46 -v --drive-server-side-across-configs --fast-list
+rclone sync gdrive01: gdrive02: --transfers 46 -v --drive-server-side-across-configs --fast-list
