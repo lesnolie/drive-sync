1126f53ae806b46845c541200d888fc0ef6db0ca
lesliesync <94773632+lesliesync@users.noreply.github.com>
2021-11-22 01:33:56 +0800

    Update wholebackup.sh

--- wholebackup.sh
+++ wholebackup.sh
@@ -1,4 +1,4 @@
 # Recommended to use OneDrive -> Google Drive and Google Drive -> Google Drive at the same time.
 # Then backup the whole backup drive...
 # Optional, if you don't need that, put a hashtag(#) before the command.
-rclone copy gdrive_black: gdrive01:Wholebackup --transfers 46 -v --drive-server-side-across-configs --fast-list
+rclone sync gdrive_black: gdrive01: --transfers 46 -v --drive-server-side-across-configs --fast-list
