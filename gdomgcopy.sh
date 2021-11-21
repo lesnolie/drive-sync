8603b9d77fbe8b3f88ed8b7c8520d75584bee903
root <root@gd.asia-east1-b.c.elated-terminal-322508.internal>
2021-11-21 18:21:53 +0000

    update

--- gdomgcopy.sh
+++ gdomgcopy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # gdsrc is source, gdrive_mirror is destination
-rclone sync gdriveomg: gdrive08 --transfers 48 -v --drive-server-side-across-configs --fast-list
+rclone copy gdriveomg: gdrive08 --transfers 48 -v --drive-server-side-across-configs --fast-list
--- wholebackup.sh
+++ wholebackup.sh
@@ -1,5 +1,6 @@
 # Recommended to use OneDrive -> Google Drive and Google Drive -> Google Drive at the same time.
 # Then backup the whole backup drive...
 # Optional, if you don't need that, put a hashtag(#) before the command.
-rclone sync gdrive_black: gdrive01: --transfers 46 -v --drive-server-side-across-configs --fast-list
-rclone copy gdrive08: gdrive01:omg  --transfers 46 -v --drive-server-side-across-configs --fast-list
+rclone sync gdrive_black: gdrive01:other --transfers 46 -v --drive-server-side-across-configs --fast-list
+rclone sync gdrive08: gdrive01:omg  --transfers 46 -v --drive-server-side-across-configs --fast-list
+# END
