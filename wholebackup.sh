2b53ee08b374de2ab92ba2c2e0b589ab32ed9508
lesliesync <94773632+lesliesync@users.noreply.github.com>
2021-11-22 02:01:01 +0800

    Update wholebackup.sh

--- wholebackup.sh
+++ wholebackup.sh
@@ -2,3 +2,4 @@
 # Then backup the whole backup drive...
 # Optional, if you don't need that, put a hashtag(#) before the command.
 rclone sync gdrive_black: gdrive01: --transfers 46 -v --drive-server-side-across-configs --fast-list
+rclone copy gdrive08: gdrive01:omg  --transfers 46 -v --drive-server-side-across-configs --fast-list
