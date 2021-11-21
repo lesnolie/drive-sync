a438811c9cbf9481e8e922aac454737150e195ad
lesliesync <94773632+lesliesync@users.noreply.github.com>
2021-11-22 02:00:01 +0800

    Update gdomgcopy.sh

--- gdomgcopy.sh
+++ gdomgcopy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # gdsrc is source, gdrive_mirror is destination
-rclone copy gdriveomg: gdrive08 --transfers 48 -v --drive-server-side-across-configs --fast-list
+rclone sync gdriveomg: gdrive08 --transfers 48 -v --drive-server-side-across-configs --fast-list
